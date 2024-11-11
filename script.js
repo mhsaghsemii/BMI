01beb759effa8a7337abad73720f56f5d0f70449
MAHSA <151853553+mhsaghsemii@users.noreply.github.com>
2024-11-11 22:13:01 +0330

    Feat(Higher BMI): Who's the person that have the higher BMI
    + Conditional Structure

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let marksHeight = 1.69
-let marksWeight = 78
+let marksHeight = 1.69;
+let marksWeight = 78;
 
-let johnHeight = 1.95
-let johnWeight = 92
+let johnHeight = 1.95;
+let johnWeight = 92;
 
-const markBmi = marksWeight / (marksHeight ** 2);
-const johnBmi = johnWeight / (johnHeight ** 2);
+const markBmi = marksWeight / marksHeight ** 2;
+const johnBmi = johnWeight / johnHeight ** 2;
 
-console.log(markBmi, johnBmi);
+if (markBmi > johnBmi) {
+  console.log(`Mark's BMI is ${markBmi} and it's higher than John's `);
+} else {
+  console.log(`John's BMI is ${johnBmi} and it's higher than Marks's `);
+}
 
-const bmi = (markBmi>johnBmi);
-console.log(bmi);
+// console.log(markBmi, johnBmi);
+
+// const bmi = markBmi > johnBmi;
+// console.log(bmi);
